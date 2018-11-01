67d05350d4ea769c954dd322e3b0713ea3a01ade
William Lawson <wjameslawson@gmail.com>
2018-11-01 18:27:49 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,213 @@
+// // This is for the homepage, it makes the image opaque when you hover over it
+// var middleImage = document.getElementById("middle-img")
+// middleImage.addEventListener("mouseover", function(){
+//     middleImage.style.opacity=0.5
+// });
+
+// middleImage.addEventListener("mouseout", function(){
+//     middleImage.style.opacity=1
+// });
+
+// This is to create an image modal for the first image in the tea gallery, so when it is clicked, it enlarges and a caption appears
+var modal = document.getElementById('myModal')
+
+var img = document.getElementById('silver-needle')
+var modalImg = document.getElementById('img01')
+var captionText = document.getElementById("caption")
+img.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+}
+
+var span = document.getElementById("close");
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// image modal for the second image
+var modal = document.getElementById('myModal')
+
+var img = document.getElementById('dragonwell')
+var modalImg = document.getElementById('img01')
+var captionText = document.getElementById("caption")
+img.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+}
+
+var span = document.getElementById("close");
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// image modal for the third image
+var modal = document.getElementById('myModal')
+
+var img = document.getElementById('sencha')
+var modalImg = document.getElementById('img01')
+var captionText = document.getElementById("caption")
+img.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+}
+
+var span = document.getElementById("close");
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// image modal for the fourth image
+var modal = document.getElementById('myModal')
+
+var img = document.getElementById('gyokuro')
+var modalImg = document.getElementById('img01')
+var captionText = document.getElementById("caption")
+img.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+}
+
+var span = document.getElementById("close");
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// image modal for the fifth image
+var modal = document.getElementById('myModal')
+
+var img = document.getElementById('hojicha')
+var modalImg = document.getElementById('img01')
+var captionText = document.getElementById("caption")
+img.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+}
+
+var span = document.getElementById("close");
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// image modal for the sixth image
+var modal = document.getElementById('myModal')
+
+var img = document.getElementById('tieguanyin')
+var modalImg = document.getElementById('img01')
+var captionText = document.getElementById("caption")
+img.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+}
+
+var span = document.getElementById("close");
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// image modal for the seventh image
+var modal = document.getElementById('myModal')
+
+var img = document.getElementById('milk-oolong')
+var modalImg = document.getElementById('img01')
+var captionText = document.getElementById("caption")
+img.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+}
+
+var span = document.getElementById("close");
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// image modal for the eight image
+var modal = document.getElementById('myModal')
+
+var img = document.getElementById('mountain-oolong')
+var modalImg = document.getElementById('img01')
+var captionText = document.getElementById("caption")
+img.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+}
+
+var span = document.getElementById("close");
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// image modal for the ninth image
+var modal = document.getElementById('myModal')
+
+var img = document.getElementById('lapsang-souchong')
+var modalImg = document.getElementById('img01')
+var captionText = document.getElementById("caption")
+img.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+}
+
+var span = document.getElementById("close");
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// image modal for the tenth image 
+var modal = document.getElementById('myModal')
+
+var img = document.getElementById('yunnan-black')
+var modalImg = document.getElementById('img01')
+var captionText = document.getElementById("caption")
+img.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+}
+
+var span = document.getElementById("close");
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// image modal for the eleventh image
+var modal = document.getElementById('myModal')
+
+var img = document.getElementById('darjeeling')
+var modalImg = document.getElementById('img01')
+var captionText = document.getElementById("caption")
+img.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+}
+
+var span = document.getElementById("close");
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// image modal for the twelth image
+var modal = document.getElementById('myModal')
+
+var img = document.getElementById('pu-erh')
+var modalImg = document.getElementById('img01')
+var captionText = document.getElementById("caption")
+img.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+}
+
+var span = document.getElementById("close");
+span.onclick = function() {
+    modal.style.display = "none";
+}
